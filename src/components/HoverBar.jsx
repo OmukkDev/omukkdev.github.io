473629ec8a795ef84cce6d28c6e030f578f71acf
shafin rahman <122025662+shafin-r@users.noreply.github.com>
2024-08-05 01:12:22 +0600

    Delete src/components/HoverBar.jsx

--- src/components/HoverBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { motion } from "framer-motion";
-
-const HoverBar = () => {
-  return <motion.div className="h-0.5 bg-black rounded-full w-0"></motion.div>;
-};
-
-export default HoverBar;
